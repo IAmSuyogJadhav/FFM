ed639b6f21ff91df6d6f8c5a17fe88cbfeab51d4
IAmSuyogJadhav <suyog.jadhav1@gmail.com>
2024-10-23 12:23:21 +0200

    Fixed the issues with the range of FFM score values.

--- ffm.py
+++ ffm.py
@@ -1,7 +1,6 @@
 import torch
 from pytorch_grad_cam import GradCAM, AblationCAM
 from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
-import matplotlib.pyplot as plt
 import numpy as np
 
 # batched dice score
@@ -12,9 +11,12 @@ def batched_dice(pred, target, eps=1e-5):
     return dice
 
 
-def compute_ffm(model, img, gt_label, target_layer, reduce='mean', top_k=3, threshold=0.5, eps=1e-5, device='cuda'):
+def compute_ffm(model, img, gt_label, target_layer, reduce='mean', top_k=3, threshold=0.5, eps=1e-5, output_softmax=False, device='cuda'):
     """
-    Compute Feature Faithfulness Metric (FFM) for a single image.
+    Compute Feature Faithfulness Metric (FFM) score for a batch of images. The model is expected to be a classification model, trained
+    to give a one-hot encoded, softmax output. If the model is not trained with softmax, set output_softmax to True and the model
+    output will be passed through a softmax layer before computing the FFM score. The FFM score is bounded between 0 and 1, where 1
+    indicates perfect faithfulness (high explainability) of the model, and 0 indicates no faithfulness (low explainability).
 
     Args:
         model: PyTorch model
@@ -25,23 +27,29 @@ def compute_ffm(model, img, gt_label, target_layer, reduce='mean', top_k=3, thre
         top_k: Number of top-k labels to consider for computing. Default: 3
         threshold: Threshold for binarizing the CAMs. Default: 0.5
         eps: Epsilon value for numerical stability. Default: 1e-5
+        output_softmax: If True, softmax is applied to the model output. Default: False
         device: Device to run the model on. Default: 'cuda'
 
     Returns:
         ffm: Feature Faithfulness Metric (FFM) score for the images in the batch.
-            Noemlised to [0, 1] where 1 means perfect faithfulness.
+            Normalised to [0, 1], higher the better.
 
     """
     # Sanity checks
     B = img.shape[0]
     assert B == len(gt_label), "Number of images and ground truth labels should be same. Make sure you are passing a batch of images."
+    if device is not None and device.startswith('cuda'):
+        assert torch.cuda.is_available(), "CUDA is not available. Set device to 'cpu' or setup CUDA."
+    assert reduce in ['mean', 'none', None], f"Invalid value for reduce: {reduce}. Expected: ['mean', 'none', None]."
 
     # Get model confidence scores for all labels
     model.eval()
     model.to(device)
     img = img.to(device)
     with torch.no_grad():
-        output = model(img).softmax(dim=1)
+        output = model(img)
+        if output_softmax:
+            output = output.softmax(dim=1)
         all_scores, all_labels = torch.topk(output, len(output[0]))  # [B, num_classes]
         all_scores = all_scores.detach().cpu().numpy(); #print('all_scores', all_scores.shape) # [B, num_classes]
         all_labels = all_labels.detach().cpu().numpy(); #print('all_labels', all_labels.shape)  # [B, num_classes]
@@ -83,11 +91,12 @@ def compute_ffm(model, img, gt_label, target_layer, reduce='mean', top_k=3, thre
 
     # FFM
     ffm_gt = gt_scores * batched_dice(gcam_gt, acam_gt, eps=eps); #print('ffm_gt', ffm_gt)
-    ffm_others = - np.sum(top_k_scores.sum(axis=1) * np.array([batched_dice(gcam_top_k[i], acam_top_k[i], eps=eps).mean() for i in range(B)]))
-
-    # print(gt_scores, top_k_scores.sum(axis=1).shape, top_k_scores.shape)
-    norm_factor = gt_scores + top_k_scores.sum(axis=1); #print('norm_factor', norm_factor)
-    ffm = (ffm_gt + ffm_others) / norm_factor
+    _other_dices = np.array([batched_dice(gcam_top_k[i], acam_top_k[i], eps=eps) for i in range(B)])
+    ffm_others = - np.sum(top_k_scores * _other_dices, axis=1); #print('_ffm_others', _ffm_others)
+    
+    # norm_factor = gt_scores + top_k_scores.sum(axis=1); print('norm_factor', norm_factor)
+    # ffm = (ffm_gt + ffm_others) / norm_factor
+    ffm = (ffm_gt + ffm_others)  # Skipping normalization
 
     # Reduction
     if reduce == 'mean':
@@ -95,7 +104,7 @@ def compute_ffm(model, img, gt_label, target_layer, reduce='mean', top_k=3, thre
     elif reduce == 'none' or reduce is None:
         pass
     else:
-        raise ValueError(f"Invalid value for reduce: {reduce}. Expected: 'mean', 'none', None.")
+        raise ValueError(f"Invalid value for reduce: {reduce}. Expected: ['mean', 'none', None].")
 
     # Normalize to 0-1
     ffm = (ffm + 1) / 2
